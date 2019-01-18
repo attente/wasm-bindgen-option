457c9d33a962da30b7f55710af3aa80423a18d86
William Hua <william@attente.ca>
2019-01-18 15:54:03 -0500

    example

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,36 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn foo() -> Option<Foo> {
+    Some(Foo::Foo)
+}
+
+#[wasm_bindgen]
+pub fn bar() -> Foo {
+    Foo::Bar
+}
+
+pub enum Foo {
+    Foo = 1,
+    Bar = 2,
+}
+
+impl wasm_bindgen::describe::WasmDescribe for Foo {
+    fn describe() {
+        wasm_bindgen::describe::inform(wasm_bindgen::describe::I32)
+    }
+}
+
+impl wasm_bindgen::convert::IntoWasmAbi for Foo {
+    type Abi = i32;
+
+    fn into_abi(self, _extra: &mut dyn wasm_bindgen::convert::Stack) -> Self::Abi {
+        self as Self::Abi
+    }
+}
+
+impl wasm_bindgen::convert::OptionIntoWasmAbi for Foo {
+    fn none() -> Self::Abi {
+        0
     }
 }
